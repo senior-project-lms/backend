734ddc05a467b93915413f8e4a74ec3e3d77a9e8
Atalay Samet Ergen <atalay.ergen@std.antalya.edu.tr>
2018-01-25 18:05:48 +0300

    Added comments for User, repo development

--- src/main/java/com/lms/controllers/UserController.java
+++ src/main/java/com/lms/controllers/UserController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = {"/api"})
 public class UserController {
@@ -29,72 +31,86 @@ public class UserController {
         return null;
     }
 
+    /**
+     * the pojo object that comes from API goes to save method of the Service
+     * check userPojo is whether null or not
+     * check the pojo which includes email, name, password, username and authority if one of them is null or empty
+     * return false, else return result of the save method of the service
+     *
+     * @param userPojo
+     * @return boolean
+     * @author atalay samet ergen
+     */
     @RequestMapping(value = {"/admin/user"}, method = RequestMethod.POST)
     public boolean saveUser(@RequestBody UserPojo userPojo) {
         try {
-            if (userPojo != null) {
-                if (userPojo.getAuthority() == null || userPojo.getAuthority().getAccessLevel() == 0) {
-                    return false;
-                }
-                if (userPojo.getEmail() == null || userPojo.getEmail().isEmpty()) {
-                    return false;
-                }
-                if (userPojo.getName() == null || userPojo.getName().isEmpty()) {
-                    return false;
-                }
-                if (userPojo.getPassword() == null || userPojo.getPassword().isEmpty()) {
-                    return false;
-                }
-                if (userPojo.getUsername() == null || userPojo.getUsername().isEmpty()) {
-                    return false;
-                }
-                if (userPojo.getUsername() == null || userPojo.getSurname().isEmpty()) {
-                    return false;
-                }
+            if (isValidUserPojo(userPojo)) {
                 return userService.save(userPojo);
-
-
             }
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return false;
-
     }
-    //@RequestMapping (value = {"/admin/users"}, method = RequestMethod.POST)
+
+    /**
+     * the pojo object list that comes from API goes to save method of the Service
+     * check userPojo list is whether null or not
+     * check the pojo list which includes emails, names, passwords, usernames and authorities and if one of them is null or empty
+     * return false, else return result of the save method of the service
+     *
+     * @param userPojos
+     * @return boolean
+     * @author atalay samet ergen
+     */
+    @RequestMapping(value = {"/admin/users"}, method = RequestMethod.POST)
+    public boolean saveUsers(@RequestBody List<UserPojo> userPojos) {
+
+        try {
+            if (userPojos == null || userPojos.size() == 0) {
+                return false;
+            }
+            for (UserPojo pojo : userPojos) {
+                if (!isValidUserPojo(pojo)) {
+                    return false;
+                }
+
+            }
+            return userService.save(userPojos);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return false;
+    }
+
+    private boolean isValidUserPojo(UserPojo userPojo) {
+        if (userPojo != null) {
+            if (userPojo.getAuthority() == null || userPojo.getAuthority().getAccessLevel() == 0) {
+                return false;
+            }
+            if (userPojo.getEmail() == null || userPojo.getEmail().isEmpty()) {
+                return false;
+            }
+            if (userPojo.getName() == null || userPojo.getName().isEmpty()) {
+                return false;
+            }
+            if (userPojo.getPassword() == null || userPojo.getPassword().isEmpty()) {
+                return false;
+            }
+            if (userPojo.getUsername() == null || userPojo.getUsername().isEmpty()) {
+                return false;
+            }
+            if (userPojo.getUsername() == null || userPojo.getSurname().isEmpty()) {
+                return false;
+            }
+            return true;
+
+
+        }
+        return false;
+    }
 }
-/*
-Post
-endpoint: ​/api/admin/user
-function​: saveUser
-parameters: ​@RequestBody ​UserPojo
-returns:​boolean
-Task: ​api ye gelen pojo nesnesi servisteki save methoduna gonderilir, hata kontrolleri yapilir
-
-
-Post
-endpoint: ​/api/admin/users
-function​: saveUsers
-parameters:​@RequestBody ​List<UserPojo>
-returns:​boolean
-Task: ​api ye gelen pojo nesneleri servisteki save methoduna gonderilir, hata kontrolleri
-yapilir
----------------------------------------------------------------------------------------------------------------------------
-
-GET
-endpoint: ​/api/admin/users
-function​: getUsers
-parameters: ​none
-returns:​​List<UserPojo>
-Task: ​Auth servisinden auth pojosuna ait ilgili pojo cekilir, ve getAllActiveUsers(authPojo)
-gonderilir, hata kontrolu yapilir, ve return islemi yapilir
-
-GET
-endpoint: ​/api/admin/user/{publicKey}
-function​: getUser
-parameters: ​@PathVariable String publicKey
-returns:​​UserPojo
-Task: ​Auth servisinden auth pojosuna ait ilgili pojo cekilir, ve getAllActiveUsers(authPojo)
-gonderilir, hata kontrolu yapilir, ve return islemi yapilir
----------------------------------------------------------------------------------------------------------------------------
- */
